c6628e0406f5c2b7049a4891eea4e0a37995c730
Luciano Franca <lucianopf@outlook.com>
2017-09-17 12:47:29 -0400

    testing speed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const retrieveBTCSummary = require('./lib/fetchData')
 
 module.exports = (req, res) => {
+  return 'Testing deploy speed'
   return retrieveBTCSummary()
     .catch(err => {
       console.log(err.response.statusMessage)
