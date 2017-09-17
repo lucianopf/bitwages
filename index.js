9cc8ce69d001713a8fafd64f6598b35e13ebf68b
Luciano Franca <lucianopf@outlook.com>
2017-09-17 13:10:51 -0400

    t1

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const retrieveBTCSummary = require('./lib/fetchData')
 
 module.exports = (req, res) => {
   return retrieveBTCSummary()
+    .then(response => Object.assign({ test: '1'}, response))
     .catch(err => {
       console.log(err.response.statusMessage)
     })
