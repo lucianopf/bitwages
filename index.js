d1ffcde26fc3029800f2c2688869e884e2192a2e
Luciano Franca <lucianopf@outlook.com>
2017-09-17 13:18:40 -0400

    fully working

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const retrieveBTCSummary = require('./lib/fetchData')
 
 module.exports = (req, res) => {
   return retrieveBTCSummary()
-    .then(response => Object.assign({ test: '2'}, response))
     .catch(err => {
       console.log(err.response.statusMessage)
     })
