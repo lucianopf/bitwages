364d25421abe93865101f61f95d237daff0f1d47
Luciano Franca <lucianopf@outlook.com>
2017-09-17 13:13:34 -0400

    trigger

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const retrieveBTCSummary = require('./lib/fetchData')
 
 module.exports = (req, res) => {
   return retrieveBTCSummary()
-    .then(response => Object.assign({ test: '1'}, response))
+    .then(response => Object.assign({ test: '2'}, response))
     .catch(err => {
       console.log(err.response.statusMessage)
     })
