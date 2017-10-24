52b544b12993874f498dd5e602d71dd5d796bfd4
Luciano Franca <lucianopf@outlook.com>
2017-10-24 15:53:30 -0400

    finalize fornt-end implementation

--- main.js
+++ main.js
@@ -1,28 +1,46 @@
-function PricesSummary() {
-  axios.get('https://bitwages.now.sh/api')
+function retrieveData() {
+  return axios.get('https://bitwages.now.sh/api')
     .then(function(response) {
-      var content = response.data.PricesSummary.map(function(item) {
-        return template(item);
-      });
-      $('.exchanges').html(content);
-      console.warn('content loaded...');
+      data = response.data
+      return data
     })
-    .catch(function(error) {
-      return error;
-    });
+    .then(function(data) {
+      console.warn('content loaded...');
+      renderPricesSummary(data)
+      renderUSDBRL(data)
+      renderUSDBRLActive(data)
+      renderUSDBRLPassive(data)
+    })
+    .catch(console.log)
 }
 
-PricesSummary();
-setInterval(PricesSummary, 60000);
+function renderPricesSummary(data) {
+  $('.results > .prices-summary > .exchanges').html(data.PricesSummary.map(template))
+}
 
-function template(item) {
+function renderUSDBRL(data) {
+  $('.results > .usdbrl > .exchanges').html(data.USDBRL.map(template))
+}
+
+function renderUSDBRLActive(data) {
+  $('.results > .usdbrl-active > .exchanges').html(data.USDBRLACTIVEACTION.map(template))
+}
+
+function renderUSDBRLPassive(data) {
+  $('.results > .usdbrl-passive > .exchanges').html(data.USDBRLPASSIVEACTION.map(template))
+}
+
+function template(item, key) {
   return `
-    <div class="col-md-6">
+    <div class="col-md-4">
     <div class="card">
-        <h4>${item.id}</h4>
+        <h4>${key + 1} - ${item.id}</h4>
         <hr class="divider">
         <h4><strong>R$ ${item.value}</strong></h4>
     </div>
     </div>
     `;
 };
+
+retrieveData()
+setInterval(retrieveData, 60000)
