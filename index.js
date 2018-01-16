a3c2c7ae01e918d1c30f0382160f65eb25409461
Luciano Franca <lucianopf@outlook.com>
2018-01-16 00:51:33 -0200

    adding new route to fetch historical data

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const retrieveBTCSummary = require('./lib/fetchData')
-const cache = new require('./lib/cache.js')({
-  fn: retrieveBTCSummary,
+const { retrieveUrl } = require('./lib/utils')
+
+const indexCache = new require('./lib/cache.js')({
+  fn: require('./lib/fetchData'),
   minutes: 1,
   log: 'Index'
 })
 
 module.exports = (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  return cache.resolve()
+  let [path, params] = retrieveUrl(req.url)
+  switch(path) {
+    case '/history': 
+      return require('./lib/db').getApiResponses(params)
+      break
+    case '/favicon.ico':
+      break
+    default:
+      return indexCache.resolve()
+      break
+  }
 }
